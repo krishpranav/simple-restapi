b542d98e0457c3deb7545377837c7ad2578f8c66
krishpranav <krisna.pranav@gmail.com>
2021-12-12 16:07:55 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+// imports
+#[macro_use]  extern crate rocket;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello World!!!!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
 }
